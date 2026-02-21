f38154bc061ee784f1c8e5075f8b5628be2d6825
YGDES <262647190+ygdes@users.noreply.github.com>
2026-02-21 06:30:16 +0100

    lfsr8.v added in /src

--- src/project.v
+++ src/project.v
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 2026 Yann Guidon
+ * Copyright (c) 2026 Yann Guidon / whygee@f-cpu.org
  * SPDX-License-Identifier: Apache-2.0
+ * Check the /doc and the diagrams
  */
 
 `default_nettype none
@@ -16,38 +17,108 @@ module tt_um_ygdes_hdsiso8 (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-//  ui[0]: "CLK_SEL"
-//  ui[1]: "EXT_CLK"
-//  ui[2]: "EXT_RST"
-//  ui[3]: "D_IN"
+////////////////////////////// Plumbing //////////////////////////////
 
-//  ui[6]: "LFSR_EN"
-//  ui[7]: "DIN_SEL"
+  // IO config & misc.
+  assign uio_oe  = 8'b11111111; // Everything goes out
 
-//  uo[0]: "D_OUT"
-//  uo[1]: "GRAY0"
-//  uo[2]: "GRAY1"
-//  uo[3]: "GRAY2"
-//  uo[4]: "CLK_OUT"
 
-//  uo[6]: "LFSR_PERIOD"
-//  uo[7]: "LFSR_BIT"
+  // General/housekeeping signals
+  wire CLK_SEL, EXT_CLK, EXT_RST, D_IN;
+  assign CLK_SEL = ui_in[0];
+  assign EXT_CLK = ui_in[1];
+  assign EXT_RST = ui_in[2];
+  assign D_IN    = ui_in[3];
 
-//  uio[0]: "PULSE0"
-//  uio[1]: "PULSE1"
-//  uio[2]: "PULSE2"
-//  uio[3]: "PULSE3"
-//  uio[4]: "PULSE4"
-//  uio[5]: "PULSE5"
-//  uio[6]: "PULSE6"
-//  uio[7]: "PULSE7"
-    
-  assign uo_out  = ui_in + 8'b01101101;
-  assign uio_out = {5'b00000, ena, clk, rst_n}; // en attendant de sortir les pulses
-  assign uio_oe  = 8'b11111111; // tout en sortie !
+  wire CLK_OUT;
+  assign uo_out[4] = CLK_OUT;
+
+
+  // SISO
+  wire [2:0] GRAY;
+  wire D_OUT;
+  assign uo_out[0] = D_OUT;
+  assign uo_out[1] = GRAY[0];
+  assign uo_out[2] = GRAY[1];
+  assign uo_out[3] = GRAY[2];
+
+  wire [7:0] PULSES;
+  assign uio_out  = PULSES;
+
+
+  // LFSR
+  wire LFSR_EN, DIN_SEL;
+  assign LFSR_EN = ui_in[6];
+  assign DIN_SEL = ui_in[7];
+
+  wire LFSR_PERIOD, LFSR_BIT;
+  assign uo_out[6] = LFSR_PERIOD;
+  assign uo_out[7] = LFSR_BIT;
+
+
+////////////////////////////// home soup //////////////////////////////
+
+  wire INT_RESET, CLK_OUT, SISO_in;
+
+  // CLK_OUT = clk if CLK_SEL=0, else EXT_CLK
+  // assign CLK_OUT = CLK_SEL ? EXT_CLK : clk;
+  (* keep *) sg13g2_mux2_2 soup_mux1(.A0(clk), .A1(EXT_CLK), .S(CLK_SEL), .X(CLK_OUT));
+
+  // Combine Reset
+  wire and_reset;
+  (* keep *) sg13g2_nand2_2 soup_nand1(.Y(and_reset), .A(rst_n), .B(EXT_RST));
+
+//  always@(posedge CLK_OUT) begin
+//    // resynch INT_RESET = rst_n AND EXT_RST
+//    INT_RESET <= rst_n & EXT_RST;
+//  end;
+  (* keep *) sg13g2_dfrbpq_2 soup_DFF1(.Q(INT_RESET), .D(and_reset), .RESET_B(1'b1), .CLK(CLK_OUT));
+
+//  always@(posedge CLK_OUT, INT_RESET) begin
+//    if (INT_RESET == 1'b0)
+//      SISO_in <= 1'b0;
+//    else
+//    // SISO_in = LFSR_BIT if DIN_SEL=1 else default D_IN
+//      SISO_in <= DIN_SEL ? LFSR_BIT : D_IN;
+//  end
+  wire mux_Din;
+  (* keep *) sg13g2_mux2_2 soup_mux2(.A0(D_IN), .A1(LFSR_BIT), .S(DIN_SEL), .X(mux_Din));
+  (* keep *) sg13g2_dfrbpq_2 soup_DFF2(.Q(SISO_in), .D(mux_Din), .RESET_B(INT_RESET), .CLK(CLK_OUT));
+
+
+////////////////////////////// sub-modules //////////////////////////////
+
+  LFSR8 lfsr(
+    // In
+    .CLK(CLK_OUT),
+    .RESET(INT_RESET),
+    .LFSR_EN(LFSR_EN),
+    // Out
+    .LFSR_PERIOD(LFSR_PERIOD),
+    .LFSR_BIT(LFSR_BIT),
+    .LFSR_STATE(PULSES));  // the LFSR state is temporarily routed to the byte output
+
+
+////////////////////////////// All the dummies go here //////////////////////////////
 
   // List all unused inputs to prevent warnings
-    // wire _unused = &{ena, clk, rst_n, 1'b0}; ==> envoyés sur uio_out
-  wire _unused = &{uio_in, 1'b0};
+  wire _unused = &{
+    ena,
+    ui_in[4],
+    ui_in[5],
+    uio_in,
+    1'b0};
+
+  assign uo_out[4] = 1'b0; // unused out
+
+  // dummy constants until I write the corresponding code
+
+  // SISO
+  assign D_OUT = 1'b0;
+  assign GRAY = 3'b000;
+
+  // LFSR
+  assign LFSR_PERIOD = LFSR_EN;
+  assign LFSR_BIT = DIN_SEL;
 
 endmodule
