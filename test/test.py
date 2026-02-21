842b41d98382af2de8a114a6b51b0c5abd1b4b96
YGDES <262647190+ygdes@users.noreply.github.com>
2026-02-21 09:12:46 +0100

    test.py: try running the LFSR

--- test/test.py
+++ test/test.py
@@ -1,12 +1,33 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
-# modified for the constant adder +109
-# by Yann Guidon
+# modified for the LFSR
+# by Yann Guidon / 2026
 
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+
+# I/O bits and constants:
+CLK_SEL     =   1  # assign CLK_SEL = ui_in[0];
+EXT_CLK     =   2  # assign EXT_CLK = ui_in[1];
+EXT_RST     =   4  # assign EXT_RST = ui_in[2];
+D_IN        =   8  # assign D_IN    = ui_in[3];
+                   # ui_in[3] unused
+SHOW_LFSR   =  32  # assign SHOW_LFSR = ui_in[5];
+LFSR_EN     =  64  # assign LFSR_EN   = ui_in[6];
+DIN_SEL     = 128  # assign DIN_SEL   = ui_in[7];
+
+D_OUT       =   1  # assign uo_out[0] = D_OUT;
+CLK_OUT     =   2  # assign uo_out[1] = CLK_OUT;
+Johnson0    =   4  # assign uo_out[2] = Johnson[0];
+Johnson1    =   8  # assign uo_out[3] = Johnson[1];
+Johnson2    =  16  # assign uo_out[4] = Johnson[2];
+Johnson3    =  32  # assign uo_out[5] = Johnson[3];
+LFSR_PERIOD =  64  # assign uo_out[6] = LFSR_PERIOD;
+LFSR_BIT    = 128  # assign uo_out[7] = LFSR_BIT;
+
+# assign uio_out  = PULSES;
 
 
 @cocotb.test()
@@ -20,30 +41,23 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    dut.ui_in.value = 0   # default is 0, disables all the circuits
+    dut.uio_in.value = 0  # will not change
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
 
     dut._log.info("Test project behavior")
 
-    dut.ui_in.value = 0
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 109
+    dut.ui_in.value = EXT_RST + LFSR_EN + SHOW_LFSR
+    await ClockCycles(dut.clk, 260)
 
-    dut.ui_in.value = 20
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 129
+#    dut.ui_in.value = 0
+#    await ClockCycles(dut.clk, 1)
+#    assert dut.uo_out.value == 109
 
-    dut.ui_in.value = 111
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 220
+#    dut.ui_in.value = 20
+#    await ClockCycles(dut.clk, 1)
+#    assert dut.uo_out.value == 129
 
-    dut.ui_in.value = 220
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 73
-
-    dut.ui_in.value = 255
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 108
