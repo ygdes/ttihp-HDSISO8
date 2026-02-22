839e9e6e2872b1abd721f5342d3e96a109778949
YGDES <262647190+ygdes@users.noreply.github.com>
2026-02-22 02:05:54 +0100

    fst -> vcd

--- test/tb.v
+++ test/tb.v
@@ -8,7 +8,7 @@ module tb ();
 
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
-    $dumpfile("tb.fst");
+     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
